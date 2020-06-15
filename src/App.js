279187c9e5aeb74155df9c054ab02ea571e8be72
preet-esh <pritesh.bhati59@gmail.com>
2020-06-15 22:46:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(props){ 
+    super(props)
+    this.state={
+    new_val:'',
+    list:[],
+}
+}
+handleSubmit=(evnt)=>{
+  evnt.preventDefault();
+  var list=this.state.list;
+  list.push(this.state.new_val);
+  this.setState({list})
+  this.setState({new_val:''})
+}
+ handleRemove=(evnt)=>{
+  evnt.preventDefault();
+  var list=this.state.list;
+  list.splice(evnt.target.id,1);
+  this.setState({list})
+ }
+render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <form>
+        <input type="text" value={this.state.new_val} onChange={(e)=>{this.setState({new_val:e.target.value})}} />
+        <button onClick={this.handleSubmit}>+</button>
+      </form>
+
+      <ol>
+       { this.state.list.map((val,i)=> <li id={i}><button id={i} onClick={this.handleRemove}>[X]</button>{val}</li>) }
+      </ol>
     </div>
   );
+}
 }
 
 export default App;
